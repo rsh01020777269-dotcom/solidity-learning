e903b6f9e16cfe34527fb0e5a0f23ae8d9460618
rsh01020777269-dotcom <rsh01020777269@gmail.com>
2026-06-28 23:29:28 +0900

    Create Storage.sol

---
+++ contracts/Storage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Storage {
+
+    uint256 value;
+
+    function set(uint256 _value) public {
+        value = _value;
+    }
+
+    function get() public view returns(uint256) {
+        return value;
+    }
+}
